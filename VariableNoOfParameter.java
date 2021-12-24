12da0a389ce6c212525eaac429df26f23488ec53
MOHAMMAD SHABBIR ALAM <93095875+imohdalam@users.noreply.github.com>
2021-12-24 15:27:13 +0530

    Create VariableNoOfParameter.java

---
+++ VariableNoOfParameter.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class Main {
+
+    static int sumAllNumber(int ... x)
+    {
+        int sum =0;
+        for(int i=0; i<x.length; i++)
+        {
+            sum = sum + x[i];
+        }
+        return sum;
+    }
+    public static void main(String[] args) {
+        System.out.println("Sum: "+sumAllNumber(5,-8,9,6,4,0));
+    }
+}
