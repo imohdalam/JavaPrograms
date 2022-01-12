96c11be521e14d8f8741d460ef3c72267b429730
MOHAMMAD SHABBIR ALAM <93095875+imohdalam@users.noreply.github.com>
2022-01-12 21:29:19 +0530

    Create TimeClass.java

---
+++ TimeClass.java
@@ -0,0 +1,33 @@
+package DateTime;
+
+import java.util.Date;
+import java.time.*;
+
+
+public class TimeClass {
+    public static void main(String[] args) {
+        Date date = new Date();
+        date.setHours(1);
+        date.setDate(6);
+        System.out.println(date);
+        LocalDate Ld = LocalDate.now(Clock.systemDefaultZone());
+        System.out.println(Ld);
+        Ld = LocalDate.now(ZoneId.of("Pacific/Apia"));
+        System.out.println(Ld);
+        Ld = LocalDate.of(2021,Month.NOVEMBER,15);
+        System.out.println(Ld);
+        Ld = LocalDate.ofEpochDay(5);
+        System.out.println(Ld);
+        Ld = LocalDate.parse("2019-02-28");
+        System.out.println(Ld);
+        LocalDate LdNew = Ld.plusMonths(2);
+        //LocalDate is immutable class
+        System.out.println(LdNew);
+
+        LocalTime Lt = LocalTime.now();
+        System.out.println(Lt);
+        LocalTime LtNew = Lt.minusHours(5);
+        // LocalTime Immutable Class
+        System.out.println(LtNew);
+    }
+}
